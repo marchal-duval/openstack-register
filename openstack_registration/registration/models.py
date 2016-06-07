8c9d7056fd337d1d65ff7c0ce00b9ce7caca4134
Gerard Marchal-Duval <marchal@lal.in2p3.fr>
2016-06-07 14:43:11 +0200

    new model

--- openstack_registration/registration/models.py
+++ openstack_registration/registration/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class UserActivation(models.Model):
+    link = models.TextField(null=False)
+    username = models.CharField(max_length=32, null=False)
+    expiration_date = models.DateField(auto_now_add=True, auto_now=False)
